a21b43cd7df8dc05ef05d5c3147175a489398e12
Ana M. Rodriguez <108434764+AnnieRod@users.noreply.github.com>
2026-05-12 14:50:02 -0500

    updated 01

---
+++ 01_exploratory_analysis.R
@@ -0,0 +1,11 @@
+
+# Exploratory plots and tables
+plot(df$date, df$deaths)
+plot(df$date, df$temp)
+plot(df$date, df$pm25)
+plot(df$temp, df$pm25)
+plot(df$deaths, df$temp)
+plot(df$deaths, df$pm25)
+
+df |> 
+  summarize(deaths = sum(deaths), temp = mean(temp))
